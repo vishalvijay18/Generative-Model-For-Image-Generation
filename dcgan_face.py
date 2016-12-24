b91f6141fc930aebc1addf4f655292659cacca3f
Vishal Vijayvargiya <vishalvijay18@gmail.com>
2016-12-24 15:40:54 -0800

    upload

---
+++ dcgan_face.py
@@ -0,0 +1,192 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Reshape
+from keras.layers.core import Flatten
+from keras.optimizers import SGD
+from keras.datasets import mnist
+import matplotlib.image as matimg
+from keras.layers.core import Activation
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import UpSampling2D
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+import cv2
+import glob
+import os
+import numpy as np
+from PIL import Image
+import argparse
+import math
+from keras.optimizers import Adam
+
+def load_image(path):
+    img = matimg.imread(path)
+    return img
+
+#following code loads data from local repo
+#directory should have two subfolders, with name train and test
+def load_local_data(path):
+    paths = glob.glob(os.path.join(path + "/train", "*.jpg"))
+    X_train = np.array( [ load_image(p) for p in paths ] )
+
+    paths = glob.glob(os.path.join(path + "/test", "*.jpg"))
+    X_test = np.array( [ load_image(p) for p in paths ] )
+   
+    return X_train, X_test
+
+#Note: This code is for 32x32 colored image
+def generator_model():
+    model = Sequential()
+    model.add(Dense(input_dim=100, output_dim=1024))
+    model.add(Activation('tanh'))
+    model.add(Dense(128*8*8))
+    model.add(BatchNormalization())
+    model.add(Activation('tanh'))
+    model.add(Reshape((128, 8, 8), input_shape=(128*8*8,)))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Convolution2D(64, 5, 5, border_mode='same'))
+    model.add(Activation('tanh'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Convolution2D(3, 5, 5, border_mode='same'))
+    model.add(Activation('tanh'))
+    return model
+
+#Note: This code is for 32x32 colored image
+def discriminator_model():
+    model = Sequential()
+    model.add(Convolution2D(
+                        64, 5, 5,
+                        border_mode='same',
+                        input_shape=(3, 32, 32)))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(128, 5, 5))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('tanh'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    return model
+
+    #dummy_input = np.ones((1, 1, 28, 28))   
+    # For TensorFlow dummy_input = np.ones((32, 12, 12, 3))
+    #preds = model.predict(dummy_input)
+    #print(preds.shape)
+
+def generator_containing_discriminator(generator, discriminator):
+    model = Sequential()
+    model.add(generator)
+    discriminator.trainable = False
+    model.add(discriminator)
+    return model
+
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num)/width))
+    generated_images = generated_images.reshape((generated_images.shape[0], 32, 32, 3) )
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1], 3),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1], 0] = \
+            img[:, :, 0]
+	image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1], 1] = \
+            img[:, :, 1]
+	image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1], 2] = \
+            img[:, :, 2]
+    return image
+
+
+def train(BATCH_SIZE):
+    path="Project/data/celebD"
+    (X_train, X_test) = load_local_data(path)
+
+    X_train = (X_train.astype(np.float32) - 127.5)/127.5
+    print X_train.shape
+    X_train = X_train.reshape((X_train.shape[0], 3) + X_train.shape[1:3])
+    print X_train.shape
+    discriminator = discriminator_model()
+    generator = generator_model()
+    discriminator_on_generator = \
+        generator_containing_discriminator(generator, discriminator)
+    d_optim = Adam(lr=0.0002, beta_1=0.5)
+    g_optim = Adam(lr=0.0002, beta_1=0.5)
+    generator.compile(loss='binary_crossentropy', optimizer=g_optim)
+    discriminator_on_generator.compile(
+        loss='binary_crossentropy', optimizer=g_optim)
+    discriminator.trainable = True
+    discriminator.compile(loss='binary_crossentropy', optimizer=d_optim)
+    noise = np.zeros((BATCH_SIZE, 100))
+
+    for epoch in range(10):
+        print("Epoch is", epoch)
+        print("Number of batches", int(X_train.shape[0]/BATCH_SIZE))
+	# load weights on first try (i.e. if process failed previously and we are attempting to recapture lost data)
+        if epoch == 0:
+            if os.path.exists('generator') and os.path.exists('discriminator'):
+                print "Loading saves weights.."
+                generator.load_weights('generator')
+                discriminator.load_weights('discriminator')
+                print "Finished loading"
+            else:
+                pass
+
+        for index in range(int(X_train.shape[0]/BATCH_SIZE)):
+            for i in range(BATCH_SIZE):
+                noise[i, :] = np.random.uniform(-1, 1, 100)
+            image_batch = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
+            generated_images = generator.predict(noise, verbose=0)
+            if index % 20 == 0:
+                image = combine_images(generated_images)
+                image = image*127.5+127.5
+                Image.fromarray(image.astype(np.uint8)).save(
+                    str(epoch)+"_"+str(index)+".png")
+            X = np.concatenate((image_batch, generated_images))
+            y = [1] * BATCH_SIZE + [0] * BATCH_SIZE
+            d_loss = discriminator.train_on_batch(X, y)
+            print("batch %d d_loss : %f" % (index, d_loss))
+            for i in range(BATCH_SIZE):
+                noise[i, :] = np.random.uniform(-1, 1, 100)
+            discriminator.trainable = False
+            g_loss = discriminator_on_generator.train_on_batch(
+                noise, [1] * BATCH_SIZE)
+            discriminator.trainable = True
+            print("batch %d g_loss : %f" % (index, g_loss))
+            if index % 10 == 9:
+                generator.save_weights('generator', True)
+                discriminator.save_weights('discriminator', True)
+
+def generate(BATCH_SIZE):
+    generator = generator_model()
+    d_optim = Adam(lr=0.0002, beta_1=0.5)
+    g_optim = Adam(lr=0.0002, beta_1=0.5)
+    generator.compile(loss='binary_crossentropy', optimizer=g_optim)
+    generator.load_weights('generator')
+    noise = np.zeros((BATCH_SIZE, 100))
+    for i in range(BATCH_SIZE):
+    	noise[i, :] = np.random.uniform(-1, 1, 100)
+    generated_images = generator.predict(noise, verbose=1)
+    image = combine_images(generated_images)
+    image = image*127.5+127.5
+    Image.fromarray(image.astype(np.uint8)).save(
+        "generated_image.png")
+
+#to run this code give this command: "python dcgan_face.py --mode train --batch_size 128"
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=str)
+    parser.add_argument("--batch_size", type=int, default=128)
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = get_args()
+    if args.mode == "train":
+        train(BATCH_SIZE=args.batch_size)
+    elif args.mode == "generate":
+        generate(BATCH_SIZE=args.batch_size)
---
+++ variational_autoencoder.py
@@ -0,0 +1,140 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from scipy.stats import norm
+import matplotlib.image as matimg
+import glob
+
+from keras.layers import Input, Dense, Lambda
+from keras.models import Model
+from keras import backend as K
+from keras import objectives
+from keras.datasets import mnist
+
+num_training_examples = 9000
+num_testing_examples = 1000
+image_vector_size = 12288 #this will be img_width X img_height X 3 (for rgb)
+
+batch_size = 100
+original_dim = 12288 #this is same as image_vector_size
+latent_dim = 2
+intermediate_dim = 512
+nb_epoch = 250
+epsilon_std = 1.0
+
+def load_image(path):
+    img = matimg.imread(path)
+    return img
+
+#following code loads data from local repo
+#directory should have two subfolders, with name train and test
+def load_local_data(path):
+    paths = glob.glob(os.path.join(path + "/train", "*.jpg"))
+    X_train = np.array( [ load_image(p) for p in paths ] )
+
+    paths = glob.glob(os.path.join(path + "/test", "*.jpg"))
+    X_test = np.array( [ load_image(p) for p in paths ] )
+   
+    return X_train, X_test
+
+#building model
+x = Input(batch_shape=(batch_size, original_dim))
+h = Dense(intermediate_dim, activation='relu')(x)
+z_mean = Dense(latent_dim)(h)
+z_log_var = Dense(latent_dim)(h)
+
+
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,
+                              std=epsilon_std)
+    return z_mean + K.exp(z_log_var / 2) * epsilon
+
+# note that "output_shape" isn't necessary with the TensorFlow backend
+z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+# we instantiate these layers separately so as to reuse them later
+decoder_h = Dense(intermediate_dim, activation='relu')
+decoder_mean = Dense(original_dim, activation='sigmoid')
+h_decoded = decoder_h(z)
+x_decoded_mean = decoder_mean(h_decoded)
+
+
+# modified calculation of loss
+def vae_loss(x, x_decoded_mean):
+    xent_loss = original_dim * objectives.binary_crossentropy(x, x_decoded_mean)
+    kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+    return xent_loss + kl_loss
+
+# vae model
+vae = Model(x, x_decoded_mean)
+vae.compile(optimizer='rmsprop', loss=vae_loss)
+
+# if want to train with data from local repo
+path="Project/data/celebB/"
+(x_train, x_test) = load_local_data(path)
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+#to train the model
+
+vae.fit(x_train, x_train,
+        shuffle=True,
+        nb_epoch=nb_epoch,
+        batch_size=batch_size,
+        validation_data=(x_test, x_test))
+
+vae.save_weights('vae_face_64_300.h5')
+
+
+#to load the already trained model weights
+#vae.load_weights('vae_face_64_300.h5')
+
+# build a model to project inputs on the latent space
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space
+'''
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+plt.figure(figsize=(6, 6))
+dummy_c = np.zeros((x_test_encoded.shape[0]))
+plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=dummy_c)
+plt.colorbar()
+plt.show()
+'''
+
+# build a face generator that can sample from the learned distribution
+decoder_input = Input(shape=(latent_dim,))
+_h_decoded = decoder_h(decoder_input)
+_x_decoded_mean = decoder_mean(_h_decoded)
+generator = Model(decoder_input, _x_decoded_mean)
+
+# display a 2D manifold of the faces
+n = 10  # figure with 10x10 faces
+face_size = 64
+#figure = np.zeros((face_size * n, face_size * n))
+# linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
+# to produce values of the latent variables z, since the prior of the latent space is Gaussian
+
+# change values below to generate new images
+grid_x = np.linspace(-10, 10, 5)
+grid_y = np.linspace(-10, 10, n)
+
+# code to create a result.png with generated faces
+plt.figure(figsize=(10, 5))
+for i, xi in enumerate(grid_x):
+    for j, yi in enumerate(grid_y):
+        z_sample = np.array([[yi, xi]])
+        x_decoded = generator.predict(z_sample)
+	x_decoded = x_decoded
+	#print x_decoded.shape
+        face = x_decoded[0].reshape(face_size, face_size, 3)
+	ax = plt.subplot(5, 10, (i*10)+j+1)
+    	plt.imshow(face)
+    	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
+plt.savefig('result.png')
